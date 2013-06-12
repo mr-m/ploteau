40ec17e830b21cd03477d566e4ec72d54440f48b
Dmitriy Trifonov <trifonov.dmitriy.m@gmail.com>
2013-06-12 15:34:48 +0700

    Change CubicSpline algorithm

--- js/cubic_spline.js
+++ js/cubic_spline.js
@@ -5,9 +5,9 @@ function CubicSpline (nodes) {
         var segments = self.segments = [];
 
         var nodes_count = nodes.length;
+        var segments_count = nodes_count - 3;
 
-        var segments_count = nodes_count - 1;
-
+        if (false) {
         // Инициализация массива сплайнов
         for (var i = 0; i < nodes_count; ++i)
         {
@@ -55,6 +55,42 @@ function CubicSpline (nodes) {
             segments[i].d = (segments[i].c - segments[i - 1].c) / hi;
             segments[i].b = hi * (2.0 * segments[i].c + segments[i - 1].c) / 6.0 + (nodes[i].y - nodes[i - 1].y) / hi;
         }
+        } else {
+            for (var i = 0; i < segments_count; i++) {
+                var x0 = nodes[i  ].x;
+                var x1 = nodes[i+1].x;
+                var x2 = nodes[i+2].x;
+                var x3 = nodes[i+3].x;
+
+                var y0 = nodes[i  ].y;
+                var y1 = nodes[i+1].y;
+                var y2 = nodes[i+2].y;
+                var y3 = nodes[i+3].y;
+
+                var M = math.matrix([
+                    [1, x0, math.pow(x0, 2), math.pow(x0, 3)],
+                    [1, x1, math.pow(x1, 2), math.pow(x1, 3)],
+                    [1, x2, math.pow(x2, 2), math.pow(x2, 3)],
+                    [1, x3, math.pow(x3, 2), math.pow(x3, 3)]
+                ]);
+
+                var M_ = math.inv(M);
+
+                var y = math.matrix([[y0], [y1], [y2], [y3]]);
+
+                var a = math.multiply(M_, y);
+
+                segments[i] = new CubicSegment();
+
+                segments[i].a = a._data[0][0];
+                segments[i].b = a._data[1][0];
+                segments[i].c = a._data[2][0];
+                segments[i].d = a._data[3][0];
+
+                segments[i].x_a = x1;
+                segments[i].x_b = x2;
+            }
+        }
     }
 
     self.segments = [];
@@ -64,7 +100,6 @@ function CubicSpline (nodes) {
         self.nodes = nodes;
         self.Build(self.nodes);
     }
-
 
     // Вычисление значения интерполированной функции в произвольной точке
     self.Interpolate = function (position) {
@@ -80,7 +115,7 @@ function CubicSpline (nodes) {
 
         if (position <= segments[0].x) // Если x меньше точки сетки x[0] - пользуемся первым эл-том массива
         {
-            s = segments[1];
+            s = segments[0];
         }
         else if (position >= segments[n - 1].x) // Если x больше точки сетки x[n - 1] - пользуемся последним эл-том массива
         {
@@ -106,8 +141,13 @@ function CubicSpline (nodes) {
             s = segments[j];
         }
 
-        var dx = position - s.x;
-        // Вычисляем значение сплайна в заданной точке по схеме Горнера (в принципе, "умный" компилятор применил бы схему Горнера сам, но ведь не все так умны, как кажутся)
-        return s.a + (s.b + (s.c / 2.0 + s.d * dx / 6.0) * dx) * dx;
+        var a = s.a;
+        var b = s.b;
+        var c = s.c;
+        var d = s.d;
+        var x = position;
+
+        var result = a + b*x + c*x*x + d*x*x*x;
+        return result;
     }
 }
